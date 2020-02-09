dc42333c085cef97d2267b6386066ed716573b83
Gosha94 <gerizch@rambler.ru>
2020-02-09 23:11:20 +0300

    Тест формы c источником данных

--- Sandbox/Form1.cs
+++ Sandbox/Form1.cs
@@ -8,13 +8,50 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Sandbox
+namespace AddBindingSource
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            // инициализируем коллекцию
+            ListData = new BindingList<MyData>();
+        }
+        /// <summary>
+        /// Коллекция значений для источника данных
+        /// </summary>
+        public BindingList<MyData> ListData { get; private set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // текстовое поле
+            TextBox tbEdit = new TextBox()
+            {
+                Dock = DockStyle.Bottom
+            };
+            // список
+            ListBox lbList = new ListBox()
+            {
+                Dock = DockStyle.Fill,
+                DisplayMember = "Value"
+            };
+            // Источник привязки данных
+            BindingSource bs = new BindingSource(this, "ListData");
+            for (int i = 0; i < 10; i++)
+                ListData.Add(new MyData() { Value = "Значение " + i });
+            // задаём источник привязки в качестве источника данных для списка
+            lbList.DataSource = bs;
+            // связываем свойство Text у текстового поля со свойством Value (класс MyData)
+            // DataSourceUpdateMode.OnPropertyChanged - моментальная реакция на изменение значения в текстовом поле
+            tbEdit.DataBindings.Add("Text", bs, "Value", false, DataSourceUpdateMode.OnPropertyChanged);
+
+            // кладём контролы на форму
+            Controls.AddRange(new Control[] { lbList, tbEdit });
+        }
+        public class MyData
+        {
+            public string Value { get; set; }
         }
     }
 }
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Sandbox
+namespace AddBindingSource
 {
     static class Program
     {
